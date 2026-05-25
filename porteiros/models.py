e618127ae19226192c6830a6929df6b82a1a1373
EmanuelCristo <emanuelcristo7@gmail.com>
2026-05-25 20:36:07 -0300

    Correção Porteiro

--- porteiros/models.py
+++ porteiros/models.py
@@ -4,8 +4,8 @@ from stdimage import StdImageField
 
 class Porteiro(models.Model):
     nome = models.CharField('Nome',max_length=50, help_text='Nome do porteiros')
-    email = models.EmailField('Email',max_length=50, help_text='Email do porteiros')
-  #  cpf = models.IntegerField('CPF', max_length=14, help_text='CPF do porteiros', unique=True)
+    email = models.EmailField('Email',max_length=50, help_text='Email do porteiros', unique=True)
+    cpf = models.CharField('CPF', max_length=14, help_text='CPF do porteiro', unique=True, null=True, blank=False)
     foto = StdImageField('Foto', upload_to='porteiros', help_text='Foto do porteiro')
 
     class Meta:
